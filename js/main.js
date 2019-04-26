bc0c6e0aff97b3ecf419847656fa36b69f7e08b2
jrogerio92 <joserogerio.net@gmail.com>
2019-04-26 20:04:18 -0300

    Upload dos Arquivos

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/* 
+    Equipe: 
+        José Rogério da Silva Júnior - Subturma 01C (Líder) 
+        Etapa 1
+*/
+
+//Valores de posição inicial
+let x = 200;
+let y = 400;
+
+let enemyx = 200
+let enemyy =  200
+
+let screenlimit = 512
+
+function setup() {
+  createCanvas(screenlimit, screenlimit);
+}
+
+function draw(){
+  
+  clear();
+
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= 5;
+  }
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    x += 5;
+  }
+
+
+  ellipse(x,y,25,25);
+  fill(0,0,200);
+
+  rect(enemyx, enemyy, 120, 40);
+  fill(200,0,0);
+
+}
